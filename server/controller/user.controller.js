360a4bc7d36cb3275ba48774b36e93ebde152d42
somcpp <saubhagya.sharma3.14@gmail.com>
2026-06-17 00:40:30 +0530

    server: Add populate enrolled courses in user

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -4,7 +4,7 @@ import { deletePhoto, uploadMedia } from "../utils/cloudinary.js";
 export const getUserProfile = async (req, res) => {
   try {
     const userId = req.id;
-    const user = await User.findById(userId).select("-password");
+    const user = await User.findById(userId).select("-password").populate("enrolledCourses");
     if(!user) {
       return res.status(404).json({
         message:"Profile not found",
