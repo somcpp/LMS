b8f4711a6f501b6e5cbdd0df8f07cc430ba71779
somcpp <saubhagya.sharma3.14@gmail.com>
2026-06-17 00:41:09 +0530

    server: Add search Controller and route

--- server/controller/course.controller.js
+++ server/controller/course.controller.js
@@ -6,9 +6,9 @@ export const createCourse = async (req, res) => {
     const { courseTitle, category } = req.body;
     if (!courseTitle || !category) {
       return res.status(400).json({
-        success: false ,
-        message: "Course title and category is required"
-      })
+        success: false,
+        message: "Course title and category is required",
+      });
     }
     const newCourse = new Course({
       courseTitle,
@@ -96,9 +96,9 @@ export const updateCourse = async (req, res) => {
   }
 };
 
-export const getCourseById = async(req,res) => {
+export const getCourseById = async (req, res) => {
   try {
-    const {courseId} = req.params;
+    const { courseId } = req.params;
     // if (!mongoose.Types.ObjectId.isValid(courseId)) {
     //   return res.status(400).json({
     //     success: false,
@@ -106,61 +106,116 @@ export const getCourseById = async(req,res) => {
     //   });
     // }
     const course = await Course.findById(courseId);
-    if(!course) {
+    if (!course) {
       return res.status(400).json({
-        success:false,
-        message: "Course Not Found"
-      })
+        success: false,
+        message: "Course Not Found",
+      });
     }
     return res.status(200).json({
       success: true,
-      course
-    })
+      course,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-      message: "Failed to get this Course details"
-    })
+      message: "Failed to get this Course details",
+    });
   }
-}
+};
 
-export const togglePublishCourse = async(req,res) => {
+export const togglePublishCourse = async (req, res) => {
   try {
-    const {courseId} = req.params;
-    const {publish} = req.query;
+    const { courseId } = req.params;
+    const { publish } = req.query;
     const course = await Course.findById(courseId);
-    if(!course) {
+    if (!course) {
       return res.status(400).json({
-        message: "course not found!"
-      })
+        message: "course not found!",
+      });
     }
 
-    course.isPublished = publish === "true"
+    course.isPublished = publish === "true";
     await course.save();
-    const statusMessage = course.isPublished? "Course Published" : "Course Unpublished";
+    const statusMessage = course.isPublished
+      ? "Course Published"
+      : "Course Unpublished";
     return res.status(200).json({
       success: true,
-      message: statusMessage
-    })
+      message: statusMessage,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-      message: "Failed to update status"
-    })
+      message: "Failed to update status",
+    });
   }
-}
+};
 
-export const getPublsihedCourses = async(req,res) => {
+export const getPublsihedCourses = async (req, res) => {
   try {
-    const courses = await Course.find({isPublished: true}).populate({path: "creator", select: "name photoURL"});
-    
+    const courses = await Course.find({ isPublished: true }).populate({
+      path: "creator",
+      select: "name photoURL",
+    });
+
     return res.status(200).json({
-      courses
-    })
+      courses,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-      message: "Failed to get courses"
-    })
+      message: "Failed to get courses",
+    });
   }
-}
+};
+
+export const searchCourse = async (req, res) => {
+  try {
+    const {
+      query = "",
+      categories = [],
+      sortByPrice = "",
+      page = 1,
+      limit = 10,
+    } = req.query;
+
+    // create search Query
+    const searchCriteria = {
+      isPublished: true,
+    };
+
+    if (query.trim()) {
+      searchCriteria.$or = [
+        { courseTitle: { $regex: query, $options: "i" } },
+        { subTitle: { $regex: query, $options: "i" } },
+        { category: { $regex: query, $options: "i" } },
+      ];
+    }
+
+    if (categories.length > 0) {
+      searchCriteria.category = { $in: categories };
+    }
+    //define sorting order
+    const sortOptions = {};
+    if (sortByPrice === "low") {
+      sortOptions.coursePrice = 1;
+    } else if (sortByPrice === "high") {
+      sortOptions.coursePrice = -1;
+    }
+
+    let courses = await Course.find(searchCriteria)
+      .populate({ path: "creator", select: "name photoURL" })
+      .sort(sortOptions);
+
+    return res.status(200).json({
+      success: true,
+      courses: courses || [],
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Some error occured while searching",
+    });
+  }
+};
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createCourse, getCourseById, getCreatorCourses, getPublsihedCourses, togglePublishCourse, updateCourse } from '../controller/course.controller.js';
+import { createCourse, getCourseById, getCreatorCourses, getPublsihedCourses, searchCourse, togglePublishCourse, updateCourse } from '../controller/course.controller.js';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 import upload from '../utils/multer.js'
 const courseRouter = express.Router();
 
 courseRouter.get('/', isAuthenticated, getCreatorCourses)
+courseRouter.get('/search', searchCourse)
 courseRouter.post('/create',isAuthenticated, createCourse);
 courseRouter.get('/published-courses', isAuthenticated, getPublsihedCourses)
 courseRouter.put('/update/:courseId', isAuthenticated,upload.single('courseThumbnail'), updateCourse)
