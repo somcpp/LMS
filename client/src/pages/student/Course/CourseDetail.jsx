2fb8ed1678b345f1a6cfa6e3242fd44fcf9f7ba8
somcpp <saubhagya.sharma3.14@gmail.com>
2026-06-17 00:35:19 +0530

    client: fix overflowing text in description

--- client/src/pages/student/Course/CourseDetail.jsx
+++ client/src/pages/student/Course/CourseDetail.jsx
@@ -70,7 +70,7 @@ const CourseDetail = () => {
         <div className="w-full lg:w-1/2 space-y-5">
           <h1 className="font-bold text-xl md:text-2xl">Description</h1>
           <p
-            className="text-sm"
+            className="prose max-w-none break-words"
             dangerouslySetInnerHTML={{ __html: course?.description }}
           />
 
