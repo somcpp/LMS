4a4b0305371c4a7cef42e94c38791a3709eddf27
somcpp <saubhagya.sharma3.14@gmail.com>
2026-06-22 17:09:24 +0530

    client: Bug fix mylearning page : is loading

--- client/src/pages/student/MyLearning/MyLeariningPage.jsx
+++ client/src/pages/student/MyLearning/MyLeariningPage.jsx
@@ -4,6 +4,7 @@ import CourseCard from '../Home/CourseCard';
 
 const MyLeariningPage = () => {
   const {data, isLoading} = useGetUserProfileQuery();
+  if(isLoading) return <h1>Loading...</h1>
   const myLearning = data?.user.enrolledCourses || [];
   return (
     <div className="max-w-4xl mx-auto my-10 px-4 md:px-0">
