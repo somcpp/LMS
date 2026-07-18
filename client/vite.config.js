3430037f0de882b793d4f81c5629e9c0f1bf8e31
somcpp <saubhagya.sharma3.14@gmail.com>
2026-07-18 21:51:08 +0530

    client: pre-deployment config

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,4 +10,20 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor';
+            }
+            if (id.includes('lucide-react') || id.includes('recharts') || id.includes('radix-ui')) {
+              return 'ui';
+            }
+          }
+        }
+      }
+    }
+  }
 })
