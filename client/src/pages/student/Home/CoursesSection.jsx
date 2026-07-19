13965a901691f167c710dc8f72e5f3e3fad2c9ca
somcpp <saubhagya.sharma3.14@gmail.com>
2026-07-19 15:09:22 +0530

    client: pre-deployment changes-2

--- client/src/pages/student/Home/CoursesSection.jsx
+++ client/src/pages/student/Home/CoursesSection.jsx
@@ -5,7 +5,6 @@ import CourseCard from "./CourseCard";
 
 const CoursesSection = () => {
   const {data, isLoading, isSuccess} = useGetPublishedCoursesQuery();
-  console.log(data);
   return (
     <div className="">
       <div className="max-w-7xl mx-auto p-6">
@@ -16,8 +15,13 @@ const CoursesSection = () => {
               <CourseSkeleton key={idx} />
             ))
           : (
-            data?.courses.length!==0 && data.courses.map((course,index) => <CourseCard key={index} course={course}/>)
+            isSuccess && data?.courses.length!==0 && data.courses.map((course,index) => <CourseCard key={index} course={course}/>)
           )}
+          {
+            isSuccess && data?.courses.length===0 && (
+              <div className="text-center text-gray-500 dark:text-gray-400">No courses found</div>
+            )
+          }
       </div>
       </div>
     </div>
